5f7b424d554b17df4b126c36276ce2a4120f98ab
rajsecretalchemist <raj@secretalchemist.com>
2026-06-02 18:35:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ logger = logging.getLogger("sa-ivr")
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+AUDIO_URL = "https://secret-alchemist-ivr-production.up.railway.app/static/greetings.mp3"
+
 @app.api_route("/answer", methods=["GET", "POST"])
 async def answer(request: Request):
     logger.info("CALL RECEIVED")
-    base_url = str(request.base_url).rstrip("/")
-    audio_url = f"{base_url}/static/greetings.mp3"
     xml = f"""<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Play>{audio_url}</Play>
+    <Play>{AUDIO_URL}</Play>
     <Hangup/>
 </Response>"""
-    logger.info(f"Returning XML with audio: {audio_url}")
+    logger.info(f"Returning XML with audio: {AUDIO_URL}")
     return Response(content=xml, media_type="application/xml")
 
 @app.api_route("/hangup", methods=["GET", "POST"])
